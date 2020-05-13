83e9099512210765d1ab1848d35a0ad151867a28
Hugo <hugo.tuil@edu.devinci.fr>
2020-05-13 17:08:20 +0200

    Skin Menu

---
+++ Assets/Hugo/Scripts/SkinMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SkinMenu : MonoBehaviour
+{
+    public static Sprite spritePlayer;
+
+    private Image imageSkin;
+    private int[] id;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        imageSkin = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChooseSkin()
+    {
+        imageSkin.sprite = EventSystem.current.currentSelectedGameObject.GetComponent<SkinObject>().spriteSkin;
+        spritePlayer = imageSkin.sprite;
+    }
+}
---
+++ Assets/Hugo/Scripts/SkinObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkinObject : MonoBehaviour
+{
+    public bool isMainSkin;
+
+    public Sprite spriteSkin { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteSkin = GetComponent<Image>().sprite;
+
+        if (isMainSkin)
+        {
+            if(SkinMenu.spritePlayer != null)
+            {
+                GetComponent<Image>().sprite = SkinMenu.spritePlayer;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
