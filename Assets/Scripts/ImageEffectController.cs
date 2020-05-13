cd8b350e8a58d653c88dde89b91b5cd383acf843
dd <jerome.braems@edu.devinci.Fr>
2020-05-13 14:39:55 +0200

    feat :100: :8ball:  ajout assets

---
+++ Assets/Scripts/ImageEffectController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageEffectController : MonoBehaviour
+{
+
+    public Material CircleMaterial, ObstacleMaterial, Skybox;
+    public float ColorShiftFactor, SkyboxShiftFactor;
+
+    private Color HSVColor = Color.clear;
+    private float ColorBalance, SkyboxRotation;
+
+    void Update()
+    {
+        UpdateColor(ColorShiftFactor);
+        RotateSkybox(SkyboxShiftFactor);
+    }
+
+    private void UpdateColor(float shift)
+    {
+        if (ColorBalance <= 1)
+            ColorBalance += Time.deltaTime * shift;
+        else
+            ColorBalance = 0;
+
+        HSVColor = Color.HSVToRGB(ColorBalance, 0.8f, 4.0f, true);
+        CircleMaterial.SetColor("_Color", HSVColor);
+        ObstacleMaterial.SetColor("_Color", HSVColor);
+    }
+
+    private void RotateSkybox(float shift)
+    {
+        if (SkyboxRotation <= 360)
+            SkyboxRotation += Time.deltaTime * shift;
+        else
+            SkyboxRotation = 0;
+
+        Skybox.SetFloat("_Rotation", SkyboxRotation);
+    }
+}
